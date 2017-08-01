0a1f24b219de50c2df6d6e4c4a6423b45d526ef7
Unknown <szymon.skrabanek@gmail.com>
2017-08-01 21:38:45 +0200

    Dodanie klasy Question

---
+++ Milionerzy/Question.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Milionerzy
+{
+    class Question
+    {
+        public string Text { get; set; }
+
+        public string AnswerA { get; set; }
+        public string AnswerB { get; set; }
+        public string AnswerC { get; set; }
+        public string AnswerD { get; set; }
+    }
+}
