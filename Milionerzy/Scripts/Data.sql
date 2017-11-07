a27859caa20902fd82b1116ec08b83948dcb1d85
Simon Skrabanek <szymon.skrabanek@gmail.com>
2017-11-07 19:17:46 +0100

    Aktualizacja skryptu danych

--- Milionerzy/Scripts/Data.sql
+++ Milionerzy/Scripts/Data.sql
@@ -5,5 +5,15 @@ INSERT INTO [Questions]
 	([Text], AnswerA, AnswerB, AnswerC, AnswerD, Answer)
 VALUES
 	('Jakiego koloru jest chaber bławatek?', 'Żółtego', 'Białego', 'Niebieskiego', 'Czerwonego', 'C'),
-	('Które z wymienionych nie jest rybą?', 'Rekin', 'Delfin', 'Flądra', 'Makrela', 'B')
+	('Które z wymienionych nie jest rybą?', 'Rekin', 'Delfin', 'Flądra', 'Makrela', 'B'),
+	('Które z poniższych nie jest owadem ani pająkiem', 'Jelonek rogacz', 'Tygrzyk paskowany', 'Czubajka', 'Komar', 'C'),
+	('Które z wymienionych anime nie należy do gatunku harem?', 'Sword Art Online', 'Kono Subarashii Sekai ni Shukufuku wo!', 'Date a Live', 'No.6', 'D'),
+	('Która z wymienionych metodologii nie dotyczy zarządzania projektami?', 'SCRUM', 'Kanban', 'Agile', 'SOLID', 'D'),
+	('W którym roku wydano komiks Tintin?', '1987', '2001', '2014', '1930', 'D'),
+	('Jak nazywa się pies Tintina?', 'Reksio', 'Kiki', 'Milou', 'Pluto', 'C'),
+	('Persymona to', 'Grzyb', 'Szczep winogron', 'Rodzaj sałaty', 'Owoc kaki', 'D'),
+	('W którym kraju można znaleźć rysunki z Nazca?', 'Peru', 'Ekwador', 'Chile', 'Gwatemala', 'A'),
+	('Jak, w mitologii rzymskiej, nazywała się bogini łowów?', 'Persefona', 'Wenus', 'Diana', 'Artemida', 'C'),
+	('Ile lat miał najstarszy flaming na świecie?', '83', '56', '37', '10', 'A'),
+	('Na terytorium którego państwa nie znajdują się góry Atlas?', 'Maroko', 'Tunezja', 'Liban', 'Algieria', 'C')
 GO
